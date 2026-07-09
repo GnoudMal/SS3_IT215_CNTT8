a0e511576454fe44629da02bbd81652ec0adabbd
LamDuong <lamduonghgvt@gmail.com>
2026-07-09 22:49:38 +0700

    second commit

--- main.py
+++ main.py
@@ -1,69 +1,45 @@
 from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel, Field
 
 app = FastAPI(title="API của Dương", description="Những API của Dương")
 
 
-books = [
-    {
-        "id": 1,
-        "title": "Python Basic",
-        "author": "Lê Minh Thu",
-        "category": "programming",
-        "year": 2022,
-        "is_available": True
-    },
-    {
-        "id": 2,
-        "title": "Web API Design",
-        "author": "Phạm Lan Hồng",
-        "category": "web",
-        "year": 2021,
-        "is_available": True
-    },
-    {
-        "id": 3,
-        "title": "Database System",
-        "author": "Lê Minh Huyền",
-        "category": "database",
-        "year": 2020,
-        "is_available": False
-    },
-    {
-        "id": 4,
-        "title": "Clean Code",
-        "author": "Lê Ánh Linh",
-        "category": "programming",
-        "year": 2008,
-        "is_available": False
-    },
-    {
-        "id": 5,
-        "title": "Computer Network",
-        "author": "Vũ Hồng Vân",
-        "category": "network",
-        "year": 2019,
-        "is_available": True
-    }
+products = [
+    {"id": 1, "name": "Keyboard", "price": 500000},
+    {"id": 2, "name": "Mouse", "price": 300000},
 ]
 
-@app.get("/health")
-def get_health():
-  return{"message": "Library API is running"}
-@app.get("/books")
-def get_book():
-  return
 
-@app.get("/books/available")
-def get_books_available():
-    books_available = []
-    for s in books:
-       if s["is_available"] == True:
-          books_available.append(s)
-    return books_available
-@app.get("/books/borrowed")
-def get_books_unavailable():
-    books_unavailable = []
-    for s in books:
-       if s["is_available"] == False:
-          books_unavailable.append(s)
-    return books_unavailable
+class ProductModel(BaseModel):
+    name: str = Field(min_length=1)
+    price: float = Field(gt=0)
+
+
+@app.post("/products")
+def add_product(product: ProductModel):
+    newData = {
+        "id": max(product["id"] for product in products) + 1,
+        "name": product.name,
+        "price": product.price,
+    }
+
+    products.append(newData)
+    return {"message": "thêm thành công", "data": newData}
+
+
+@app.get("/products")
+def get_all_pro():
+    if not products:
+        return {"message": "Danh sách rỗng!"}
+
+    return products
+
+
+@app.delete("/products/{product_id}")
+def delete_product(product_id: int):
+    for product in products:
+        if product["id"] == product_id:
+            products.remove(product)
+            return {"Message": "Xóa thành công"}
+    return {"Message": "Không tìm thấy sản phẩm"}
